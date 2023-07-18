327d9646dbfa418ade484c9d9ef2b4fe84268c82
MARY NJOKI <marynjoki738@gmail.com>
2023-07-18 13:01:11 +0300

    event-listening

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
-}
+    const input = document.getElementById('input');
+    input.addEventListener('click', function() {
+      console.log('I was clicked');
+    });
+  }
+  
+  addingEventListener(); // Call the function to bind the event listener
+  
